834d1d98e80a23bcfc2199abaa7a58dccc21032d
Rodion Gudz <rgudz@gmail.com>
2026-09-22 18:14:02 +0300

    unleash 2.1.1

--- Formula/unleash.rb
+++ Formula/unleash.rb
@@ -1,8 +1,8 @@
 class Unleash < Formula
   desc "Single-script MDM bypass and suppression for macOS (fork with fixes)"
   homepage "https://github.com/rodion-gudz/unleash"
-  url "https://github.com/rodion-gudz/unleash/releases/download/v2.1.0/unleash-standalone.sh"
-  sha256 "45b580550d546427f0dbc4075977194b3468e05fc4f79af23caa65cdd165d085"
+  url "https://github.com/rodion-gudz/unleash/releases/download/v2.1.1/unleash-standalone.sh"
+  sha256 "ef3806d0552b841834452aa5498f24868e6ca30d2783e6f868d01eafc076bb26"
   license "MIT"
 
   def install
